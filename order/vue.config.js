5803ddae0e5cb1cae929df7a82b650149b929cef
hannah <hanforwork896@gmail.com>
2022-12-18 02:05:34 +0800

    fix:vue.config.js

--- order/vue.config.js
+++ order/vue.config.js
@@ -1,13 +1,6 @@
-// const { defineConfig } = require("@vue/cli-service");
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   lintOnSave: fals
-//   devServer:{
-//     disAbleHostCheck:true,
-//   }
-// });
-module.exports = {
-  devServer:{
-    disableHostCheck:true,
-  }
-};
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false
+
+});
